54ac384af796461962e4060110c470c794829664
Anselm Baur <anselm.baur@desy.de>
2021-01-15 14:10:29 +0100

    extended the result search

--- wrapper.py
+++ wrapper.py
@@ -8,7 +8,8 @@ class WolframAlpha:
         self.client = wa.Client(self.app_id)
         self.result = None
         self.cmd = None
-
+    
+    
     def query(self,cmd):
         self.cmd = cmd
         print("query: "+cmd)
@@ -16,14 +17,24 @@ class WolframAlpha:
 
         return self._parse_result(self.result)
 
-        
+
     def _parse_result(self,result):
-        for r in result.results:
+        for r in result.pods:
+            #print(f"found @title: {r['@title']}")
             if r["@title"] == "Solution":
                 for sub in r.subpod:
                     res_str = sub.plaintext
                     res_str = res_str.replace("sqrt", "*sqrt")
                     res_value = eval(res_str.replace('x = ', ''))
+                    print(res_str)
+                    print(f"x = {res_value}")
+                    return float(res_value)
+
+            if r["@title"] == "Numerical solution":
+                for sub in r.subpod:
+                    res_str = sub.plaintext
+                    res_str = res_str.replace("sqrt", "*sqrt")
+                    res_value = eval((res_str.replace('x ≈ ', '')).replace("...",""))
                     print(res_str)
                     print(f"x = {res_value}")
                     return float(res_value)
