60475ab25d0e20804b504aaf9bf0c5658ee0dbdc
Anselm Baur <anselm.baur@desy.de>
2021-01-15 13:21:16 +0100

    initial commit

---
+++ wrapper.py
@@ -0,0 +1,32 @@
+import wolframalpha as wa
+from numpy import sqrt
+
+
+class WolframAlpha:
+    def __init__(self, app_id):
+        self.app_id = app_id
+        self.client = wa.Client(self.app_id)
+        self.result = None
+        self.cmd = None
+
+    def query(self,cmd):
+        self.cmd = cmd
+        print("query: "+cmd)
+        self.result = self.client.query(cmd)
+
+        return self._parse_result(self.result)
+
+        
+    def _parse_result(self,result):
+        for r in result.results:
+            if r["@title"] == "Solution":
+                for sub in r.subpod:
+                    res_str = sub.plaintext
+                    res_str = res_str.replace("sqrt", "*sqrt")
+                    res_value = eval(res_str.replace('x = ', ''))
+                    print(res_str)
+                    print(f"x = {res_value}")
+                    return float(res_value)
+
+    def get_last_result(self):
+        return self._parse_result(self.result)
